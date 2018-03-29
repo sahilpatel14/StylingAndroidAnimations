1d5e1e5a83cf03ad49669b1163810dd4b668c214
Sahil Patel <sahil-mac@Sahils-MacBook-Pro.local>
2018-03-29 10:34:19 +0530

    Initial commit

---
+++ app/src/main/java/com/ninthsemester/recyclerviewanimations/MyAdapter.kt
@@ -0,0 +1,71 @@
+package com.ninthsemester.recyclerviewanimations
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by sahil-mac on 28/03/18.
+ */
+class MyAdapter(private val string: String) : RecyclerView.Adapter<MyAdapter.ViewHolder>(){
+
+    private val items: MutableList<String> = mutableListOf()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_item, parent, false)
+                    .run {
+                        ViewHolder(this)
+                    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
+
+    private fun uniqueString(base: String) = "$base ${(Math.random() * 1000).toInt()}"
+
+    fun appendItem(newString: String) =
+            items.add(uniqueString(newString)).also {
+                notifyItemInserted(itemCount - 1)
+            }
+
+    inner class ViewHolder(
+            itemView: View,
+            private val textView : TextView = itemView.findViewById(android.R.id.text1),
+            addButton : View = itemView.findViewById(R.id.add),
+            removeButton : View = itemView.findViewById(R.id.remove)
+    ) : RecyclerView.ViewHolder(itemView) {
+
+
+        init {
+            addButton.setOnClickListener(insert())
+            removeButton.setOnClickListener(remove())
+        }
+
+
+        private fun insert() : (View) -> Unit = {
+
+            layoutPosition.also { currentPosition ->
+                items.add(currentPosition, uniqueString(string))
+                notifyItemInserted(currentPosition)
+            }
+        }
+
+
+        private fun remove() : (View) -> Unit = {
+
+            layoutPosition.also { currentPosition ->
+                items.removeAt(currentPosition)
+                notifyItemRemoved(currentPosition)
+            }
+
+        }
+
+        fun bind(text : String) {
+            textView.text = text
+        }
+    }
+}
