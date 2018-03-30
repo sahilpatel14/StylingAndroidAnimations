41c3d878c1b0469f3ba24cc6247a84b4a2a5b36d
Sahil Patel <sahil-mac@Sahils-MacBook-Pro.local>
2018-03-30 19:43:09 +0530

    added example for vector drawable and animated vector drawable

--- app/src/main/java/com/ninthsemester/stylingandroidanimations/MainActivity.kt
+++ app/src/main/java/com/ninthsemester/stylingandroidanimations/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.ninthsemester.stylingandroidanimations
 
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.graphics.drawable.Animatable2Compat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawable = imageView.drawable
+        if (drawable is Animatable) {
+            (drawable as Animatable).start()
+        }
     }
 }
