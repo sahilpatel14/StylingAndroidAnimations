8b3144350b247d1fff34f57caee18660e836ff53
Sahil Patel <sahil-mac@Sahils-MacBook-Pro.local>
2018-03-31 19:20:59 +0530

    adding more than one transition into a transition set

--- app/src/main/java/com/ninthsemester/stylingandroidanimations/MainActivity.kt
+++ app/src/main/java/com/ninthsemester/stylingandroidanimations/MainActivity.kt
@@ -3,9 +3,7 @@ package com.ninthsemester.stylingandroidanimations
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.ChangeBounds
-import android.transition.Scene
-import android.transition.TransitionManager
+import android.transition.*
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridLayout
@@ -21,7 +19,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mScene1: Scene
     private lateinit var mScene2: Scene
-    private lateinit var mScene3: Scene
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,32 +30,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             mContainer = container
             mTransitionManager = TransitionManager()
 
-            mScene1 = Scene.getSceneForLayout(mContainer, R.layout.grid1, this)
-            mScene2 = Scene.getSceneForLayout(mContainer, R.layout.grid2, this)
-            mScene3 = Scene.getSceneForLayout(mContainer, R.layout.grid3, this)
+            mScene1 = Scene.getSceneForLayout(mContainer, R.layout.part3_1, this)
+            mScene2 = Scene.getSceneForLayout(mContainer, R.layout.part3_2, this)
 
             mScene1.setEnterAction {
-                mScene1.sceneRoot.findViewById<View>(R.id.item2).setOnClickListener(this)
-                mScene1.sceneRoot.findViewById<View>(R.id.item3).setOnClickListener(this)
+                mScene1.sceneRoot.findViewById<View>(R.id.item_1c).setOnClickListener(this)
             }
 
             mScene2.setEnterAction {
-                mScene2.sceneRoot.findViewById<View>(R.id.item1).setOnClickListener(this)
-                mScene2.sceneRoot.findViewById<View>(R.id.item3).setOnClickListener(this)
+                mScene2.sceneRoot.findViewById<View>(R.id.item_1a).setOnClickListener(this)
             }
 
-            mScene3.setEnterAction {
-                mScene3.sceneRoot.findViewById<View>(R.id.item1).setOnClickListener(this)
-                mScene3.sceneRoot.findViewById<View>(R.id.item2).setOnClickListener(this)
-            }
 
-            val transition = ChangeBounds()
+//            val transition = AutoTransition()
+//            or
+            val transition = TransitionSet()
+            transition.ordering = TransitionSet.ORDERING_TOGETHER
+            transition.addTransition(ChangeBounds()).addTransition(Fade())
             mTransitionManager.setTransition(mScene1, mScene2, transition)
-            mTransitionManager.setTransition(mScene1, mScene3, transition)
             mTransitionManager.setTransition(mScene2, mScene1, transition)
-            mTransitionManager.setTransition(mScene2, mScene3, transition)
-            mTransitionManager.setTransition(mScene3, mScene1, transition)
-            mTransitionManager.setTransition(mScene3, mScene2, transition)
             mScene1.enter()
 
         }
@@ -70,9 +60,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             when (v?.id) {
-                R.id.item1 -> mTransitionManager.transitionTo(mScene1)
-                R.id.item2 -> mTransitionManager.transitionTo(mScene2)
-                R.id.item3 -> mTransitionManager.transitionTo(mScene3)
+                R.id.item_1a -> mTransitionManager.transitionTo(mScene1)
+                R.id.item_1c -> mTransitionManager.transitionTo(mScene2)
             }
         }
     }
