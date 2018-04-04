1554b20fa108ad64e075167e626c5823201ca885
Sahil Patel <sahil-mac@Sahils-MacBook-Pro.local>
2018-04-04 09:31:13 +0530

    added example for converting an activity into dialog using a custom theme

--- app/src/main/java/com/ninthsemester/stylingandroidanimations/MainActivity.kt
+++ app/src/main/java/com/ninthsemester/stylingandroidanimations/MainActivity.kt
@@ -2,6 +2,9 @@ package com.ninthsemester.stylingandroidanimations
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+
+//  https://blog.stylingandroid.com/vector-drawables-part-3/
 
 class MainActivity : AppCompatActivity() {
 
